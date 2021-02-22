f4dfc10119d9446ad806b69965aee2a7be3b0420
Qing Ma <gsmofgsm@hotmail.com>
2021-02-22 14:32:18 +0100

    change serializer setting in .AddMvc().AddJsonOptions to make json key Camel case

--- CityInfo.Api/Startup.cs
+++ CityInfo.Api/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +16,16 @@ namespace CityInfo.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                .AddJsonOptions(o =>
+                {
+                    if (o.SerializerSettings.ContractResolver != null)
+                    {
+                        var castedResolver = o.SerializerSettings.ContractResolver
+                                                as DefaultContractResolver;
+                        castedResolver.NamingStrategy = null;
+                    }
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
