806b43407ddbd6d4cc02e2629245dbde4c61ead9
Qing Ma <gsmofgsm@hotmail.com>
2021-02-22 12:53:22 +0100

    return resources

---
+++ CityInfo.Api/Controllers/CitiesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityInfo.Api.Controllers
+{
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>()
+                {
+                    new { id = 1, Name = "New York City" },
+                    new { id = 2, Name = "Antwerp" },
+                });
+        }
+    }
+}
