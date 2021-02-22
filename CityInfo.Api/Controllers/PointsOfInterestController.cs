9f473dd310fb54e448dc38c23c5ed85bd7147dfa
Qing Ma <gsmofgsm@hotmail.com>
2021-02-22 17:19:19 +0100

    delete a resource

--- CityInfo.Api/Controllers/PointsOfInterestController.cs
+++ CityInfo.Api/Controllers/PointsOfInterestController.cs
@@ -201,5 +201,30 @@ namespace CityInfo.Api.Controllers
 
             return NoContent();
         }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletePointOfInterest(int cityId, int id)
+        {
+            var city = CityDataStore.Current.Cities
+                   .FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            // find point fo interest
+            var pointOfInterestFromStore = city.PointsOfInterest
+                .FirstOrDefault(p => p.Id == id);
+
+            if (pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            city.PointsOfInterest.Remove(pointOfInterestFromStore);
+
+            return NoContent();
+        }
     }
 }
