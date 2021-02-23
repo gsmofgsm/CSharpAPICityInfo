4e83561aa697b7c01e1c5bc9c71d336312837ca0
Qing Ma <gsmofgsm@hotmail.com>
2021-02-23 21:41:47 +0100

    UseNLog for logging after bookstrapped

--- CityInfo.Api/Program.cs
+++ CityInfo.Api/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -19,6 +20,7 @@ namespace CityInfo.Api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseNLog();
     }
 }
