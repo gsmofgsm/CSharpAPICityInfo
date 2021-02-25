acd7519e8c79059269b2a728a5dd05172c25dbf0
Qing Ma <gsmofgsm@hotmail.com>
2021-02-25 19:23:32 +0100

    Update Table Add Column migration

--- CityInfo.Api/Entities/PointOfInterest.cs
+++ CityInfo.Api/Entities/PointOfInterest.cs
@@ -16,6 +16,8 @@ namespace CityInfo.Api.Entities
         [Required]
         [MaxLength(50)]
         public string Name { get; set; }
+        [MaxLength(200)]
+        public string Description { get; set; }
         [ForeignKey("CityId")]
         public City City { get; set; }
         public int CityId { get; set; } // not needed but recommended
--- CityInfo.Api/Migrations/20210225182014_CityInfoDBAddPoIDescription.Designer.cs
+++ CityInfo.Api/Migrations/20210225182014_CityInfoDBAddPoIDescription.Designer.cs
@@ -3,14 +3,16 @@ using CityInfo.Api.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CityInfo.Api.Migrations
 {
     [DbContext(typeof(CityInfoContext))]
-    partial class CityInfoContextModelSnapshot : ModelSnapshot
+    [Migration("20210225182014_CityInfoDBAddPoIDescription")]
+    partial class CityInfoDBAddPoIDescription
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
